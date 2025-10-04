8a72610a38bc841f27bbd318df3a4eae8c60d5b3
Vilius Valinskis <vilius.valinsk@gmail.com>
2025-10-04 23:12:11 +0300

    fat jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("java")
     kotlin("jvm")
@@ -31,6 +33,38 @@ tasks.test {
     useJUnitPlatform()
 }
 
+// fat jar build task
+tasks.register<Jar>("fatJar") {
+    group = "build"
+    description = "Assembles a fat jar archive containing the main classes and their dependencies."
+
+    archiveClassifier.set("all")
+
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    val sourcesMain = sourceSets.main.get()
+    val contents = configurations.runtimeClasspath.get()
+        .map {
+            if (it.isDirectory) it else zipTree(it)
+        } + sourcesMain.output
+
+    from(contents)
+
+    manifest {
+        attributes["Main-Class"] = application.mainClass.get()
+    }
+
+    tasks.assemble {
+        dependsOn(this)
+    }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+
+}
+
 kotlin {
-    jvmToolchain(21)
+    jvmToolchain(17)
 }
